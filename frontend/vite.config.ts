6408de5eabe0af3b4fa9d98e93a0269299cab875
nihi-lo <115719300+nihi-lo@users.noreply.github.com>
2024-02-15 22:39:05 +0900

    ➕ Vitest を導入

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -5,4 +7,13 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.ts",
+    coverage: {
+      enabled: true,
+      include: ["src/**/*.{ts,tsx}"],
+    },
+  },
 });
