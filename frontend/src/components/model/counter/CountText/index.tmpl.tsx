ebc99225d5adb60f41da6a374304b836a3571698
nihi-lo <115719300+nihi-lo@users.noreply.github.com>
2024-03-28 02:23:23 +0900

    🔥 Counterコンポーネントを削除

--- frontend/src/components/model/counter/CountText/index.tmpl.tsx
+++
@@ -1,7 +0,0 @@
-import { useCountStore } from "@{{.ProjectName}}/hooks/useCountStore";
-
-export const CountText = (): JSX.Element => {
-  const count = useCountStore((state) => state.count);
-
-  return <div className="w-8 text-center text-xl">{count}</div>;
-};
